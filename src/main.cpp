8780bed8d87d9c1eda3741a441187c0aa21245c4
Charles Adair <charlie@adair.sh>
2026-09-15 14:40:25 -0600

    Seperated logic for input handling to address a bug + Added text files for the three provided test cases.

--- src/main.cpp
+++ src/main.cpp
@@ -11,17 +11,13 @@ int main() {
     int n, p;
     std::vector<int> damageAmmounts;
 
-    // Input handling
-    std::cin >> n >> p;
-    int damageVal;
-    while (std::cin >> damageVal) { damageAmmounts.push_back(damageVal); }
-
-    // Data Validation
+    // Input handling for n,p
     if (!(std::cin >> n >> p)) {
         std::cerr << "Expected two integers.\n";
         return 1;
     }
 
+    // Data validation for n,p
     if (n < 1 || n > 100000) {
         std::cerr << "n must be between 1 and 100000. Was " << n << '\n';
         return 1;
@@ -32,13 +28,18 @@ int main() {
         return 1;
     }
 
+    // Input handling for the damage at each location
+    int damageVal;
+    while (std::cin >> damageVal) { damageAmmounts.push_back(damageVal); }
+
+    // Data validation for damage at each location (must contain n ints)
     if (damageAmmounts.size() != n) {
     std::cerr << "Expected " << n
               << " values, but received " << damageAmmounts.size() << '\n';
     return 1;
-}
+    }
 
-
+    std::cout << "1";
 
     return 0;
 }
