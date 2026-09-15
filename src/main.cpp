71a1fdd7998ec3323facc94a283de23adc9e6a32
Charles Adair <charlie@adair.sh>
2026-09-15 14:01:52 -0600

    Added input handling logic

---
+++ src/main.cpp
@@ -0,0 +1,44 @@
+// Rabbit-Roundup Implemenation
+#include <vector>
+#include <string>
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+
+int main() {
+
+    // Input values
+    int n, p;
+    std::vector<int> damageAmmounts;
+
+    // Input handling
+    std::cin >> n >> p;
+    int damageVal;
+    while (std::cin >> damageVal) { damageAmmounts.push_back(damageVal); }
+
+    // Data Validation
+    if (!(std::cin >> n >> p)) {
+        std::cerr << "Expected two integers.\n";
+        return 1;
+    }
+
+    if (n < 1 || n > 100000) {
+        std::cerr << "n must be between 1 and 100000. Was " << n << '\n';
+        return 1;
+    }
+
+    if (p < 1 || p > 1000) {
+        std::cerr << "p must be between 1 and 1000. Was " << p << '\n';
+        return 1;
+    }
+
+    if (damageAmmounts.size() != n) {
+    std::cerr << "Expected " << n
+              << " values, but received " << damageAmmounts.size() << '\n';
+    return 1;
+}
+
+
+
+    return 0;
+}
